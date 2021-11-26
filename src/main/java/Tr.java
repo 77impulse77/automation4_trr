d5e2d8ece72067301c5b7394140656a00c8d298e
Vyacheslav <scugorshkof@gmail.com>
2021-11-27 02:08:00 +0300

    1

--- src/main/java/Tr.java
+++ src/main/java/Tr.java
@@ -12,24 +12,26 @@ public class Tr {
 
     }
 
-    public static void tr(int a, int b, int c) {
+    public static double tr(int a, int b, int c) {
 
         double p = ((float)(a+b+c))/2;
 
         if (p <= a || p <= b || p <= c || a*b*c==0) {
-            System.out.println(" Это не треугольник");
-        return;
+//            System.out.println(" Это не треугольник");
+        return -1;
         }
         double S = Math.sqrt(p*(p - a)*(p - b)*(p - c));
 
-        if (a == b && b == c) {
-            System.out.println(" Площадь равна " + S + " Это равностороний треугольник");
-        } else if (a==b || a==c || b==c) {
-            System.out.println(" Площадь равна " + S + " Это равнобедренный треугольник");
-        } else if (S == 0.5*a*b || S == 0.5*a*c || S == 0.5*c*b){
-            System.out.println(" Площадь равна " + S + " Это прямоугольный треугольник");
-        } else {
-            System.out.println(" Площадь равна " + S + " Это разносторонний треугольник");
-        }
+//        if (a == b && b == c) {
+//            System.out.println(" Площадь равна " + S + " Это равностороний треугольник");
+//        } else if (a==b || a==c || b==c) {
+//            System.out.println(" Площадь равна " + S + " Это равнобедренный треугольник");
+//        } else if (S == 0.5*a*b || S == 0.5*a*c || S == 0.5*c*b){
+//            System.out.println(" Площадь равна " + S + " Это прямоугольный треугольник");
+//        } else {
+//            System.out.println(" Площадь равна " + S + " Это разносторонний треугольник");
+//        }
+        return S;
     }
+
 }
